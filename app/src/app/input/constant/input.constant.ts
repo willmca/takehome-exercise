7930c7b3b6ede6c632645cd27fccc914409157f5
Will McAllister <53065222+willmca@users.noreply.github.com>
2024-08-12 21:37:58 -0400

    some styling, setup input options

---
+++ app/src/app/input/constant/input.constant.ts
@@ -0,0 +1,16 @@
+import { SelectItem } from "primeng/api";
+
+export const INPUT_OPTIONS: SelectItem[] = [
+    {
+        label: '250k',
+        value: '250,000'
+    },
+    {
+        label: '10m',
+        value : '10,000,000'
+    },
+    {
+        label: '.5b',
+        value: '500,000,000'
+    }
+]
--- app/src/app/input/input.component.ts
+++ app/src/app/input/input.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { INPUT_OPTIONS } from './constant/input.constant';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-input',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './input.component.scss'
 })
 export class InputComponent {
+  constructor() {
 
+  }
+
+  public inputOptions: SelectItem[] = INPUT_OPTIONS
 }
