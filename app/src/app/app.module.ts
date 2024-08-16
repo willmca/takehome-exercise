8c45a670854a100da44f4f6e23dfdcde6fe0660a
Will McAllister <53065222+willmca@users.noreply.github.com>
2024-08-15 23:00:00 -0400

    Update app.module.ts
    remove unused modules

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,13 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutesModule } from './app.routes';
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './components/display/display.component';
-import { DropdownModule } from 'primeng/dropdown';
 import { RouterOutlet } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { valueReducer } from './store/reducers/value.reducer';
 import { ButtonModule } from 'primeng/button';
-import { FloatLabelModule } from 'primeng/floatlabel';
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { DirectivesModule } from './directives/directives.module';
 
@@ -24,10 +21,7 @@ import { DirectivesModule } from './directives/directives.module';
     BrowserModule,
     FormsModule,
     AppRoutesModule,
-    DropdownModule,
     RouterOutlet,
-    BrowserAnimationsModule,
-    FloatLabelModule,
     ButtonModule,
     StoreModule.forRoot({ valueState: valueReducer }),
     DirectivesModule
