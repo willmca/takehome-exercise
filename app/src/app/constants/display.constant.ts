54fca532332df1f662bed2b6ddef4c35b83d71d9
Will McAllister <53065222+willmca@users.noreply.github.com>
2024-08-15 22:28:55 -0400

    Update display.constant.ts
    const works to properly format values of k, m, b

--- app/src/app/constants/display.constant.ts
+++ app/src/app/constants/display.constant.ts
@@ -1,18 +1,23 @@
 import { includes } from "lodash-es";
 
 export function GET_DISPLAY_FROM_INPUT(input: string): string {
-    const suffix = input.charAt(input.length - 1);
-    let formattedValue = input;
-    const includesDecimal = includes(input, '.');
+    const suffix: string = input.charAt(input.length - 1);
+    let formattedValue: string = input;
+    const includesDecimal: boolean = includes(input, '.');
     if (suffix) {
         switch (suffix) {
             case 'k': {
+                // remove the suffix
                 formattedValue = formattedValue.slice(0, -1);
+                // if no decimal, just add 0s and commas
                 if (!includesDecimal) {
                     formattedValue += ',000'
                 } else {
-                    const splitValueByDecimal = formattedValue.split('.')
-                    const zeroesToAdd = 3 - splitValueByDecimal[1].length;
+                    // if decimal, split into 2 strings, before and after decimal
+                    const splitValueByDecimal: string[] = formattedValue.split('.')
+                    // 3 chars after decimal, zeroes to add + numbers after decimal should add to 3
+                    const zeroesToAdd: number = 3 - splitValueByDecimal[1].length;
+                    // Replace the decimal with a comma and add neeeded zeroes
                     formattedValue = formattedValue.replace('.', ',')
                     formattedValue += '0'.repeat(zeroesToAdd);
                 }
@@ -23,10 +28,11 @@ export function GET_DISPLAY_FROM_INPUT(input: string): string {
                 if (!includesDecimal) {
                     formattedValue += ',000,000'
                 } else {
-                    const splitValueByDecimal = formattedValue.split('.')
-                    const zeroesToAdd = 6 - splitValueByDecimal[1].length;
+                    const splitValueByDecimal: string[] = formattedValue.split('.')
+                    const zeroesToAdd: number = 6 - splitValueByDecimal[1].length;
                     formattedValue = formattedValue.replace('.', ',')
-                    formattedValue += '0'.repeat(zeroesToAdd);
+                    formattedValue += '0'.repeat(zeroesToAdd - 3);
+                    formattedValue += ',000'
                 }
                 break
             }
@@ -35,10 +41,11 @@ export function GET_DISPLAY_FROM_INPUT(input: string): string {
                 if (!includesDecimal) {
                     formattedValue += ',000,000,000'
                 } else {
-                    const splitValueByDecimal = formattedValue.split('.')
-                    const zeroesToAdd = 9 - splitValueByDecimal[1].length;
+                    const splitValueByDecimal: string[] = formattedValue.split('.')
+                    const zeroesToAdd: number = 9 - splitValueByDecimal[1].length;
                     formattedValue = formattedValue.replace('.', ',')
-                    formattedValue += '0'.repeat(zeroesToAdd);
+                    formattedValue += '0'.repeat(zeroesToAdd - 6);
+                    formattedValue += ',000,000'
                 }
                 break
             }
