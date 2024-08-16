45c68a9aa9c0ca096c866acd8fa31de995002a33
Will McAllister <53065222+willmca@users.noreply.github.com>
2024-08-15 23:03:33 -0400

    Update directives.module.ts

--- app/src/app/directives/directives.module.ts
+++ app/src/app/directives/directives.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FinancialInputsDirective } from './financial-inputs.directive';
+import { FinancialInputsDirective } from './financial-inputs/financial-inputs.directive';
 
 
 
